e0af56303c4748e6237fa54eaac47a4c136abc8d
marinamitunevich <marina.mitunevich@gmail.com>
2021-06-09 21:47:58 +0300

    new changes

--- src/cw/otherHW/Filter.java
+++ src/cw/otherHW/Filter.java
@@ -5,29 +5,14 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Filter {
-    public List<Account> filter(List<Account> accounts, int conditionNummer){
+    public List<Account> filter(List<Account> accounts, Predicate<Account> predicate) {
         List<Account> newList = new ArrayList<>();
-        if(conditionNummer == 1) {
-            Predicate<Long> predicate = n -> n > 0;
-            for (int i = 0; i < accounts.size(); i++) {
-                if (predicate.test(accounts.get(i).getBalance())) {
-                    newList.add(accounts.get(i));
 
-                }
+        for (int i = 0; i < accounts.size(); i++) {
+            if (predicate.test(accounts.get(i))) {
+                newList.add(accounts.get(i));
             }
         }
-
-        if(conditionNummer == 2) {
-            for (int i = 0; i < accounts.size(); i++) {
-                OurPredicate<Long,Boolean> predicate2 = (n,m) -> n > 100_000 && m == false;
-                if (predicate2.test(accounts.get(i).getBalance(),accounts.get(i).isLocked())) {
-                    newList.add(accounts.get(i));
-
-                }
-            }
-        }
-
         return newList;
-
     }
 }
--- src/cw/otherHW/Runner.java
+++ src/cw/otherHW/Runner.java
@@ -12,12 +12,15 @@ public class Runner {
                 new Account("10",-100_000,true),
                 new Account("05",10_000,false));
 
+        Predicate<Account> byBalance = account -> account.getBalance() > 0;
+        Predicate<Account> byBalanceAndLocked = account -> account.getBalance() > 100_000 && account.isLocked() == false;
+
 
         System.out.println("List od the accounts that satisfies filter2 :");
-        System.out.println((new Filter()).filter(accounts,2));
+        System.out.println((new Filter()).filter(accounts,byBalanceAndLocked));
 
         System.out.println("List od the accounts that satisfies filter1 :");
-        System.out.println((new Filter()).filter(accounts,1));
+        System.out.println((new Filter()).filter(accounts,byBalance));
 
     }
 }
